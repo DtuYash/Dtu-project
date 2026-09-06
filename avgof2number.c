130fa57bb03a740a5c270898e7f60262dd890d0b
Yash <yashdpps@gmail.com>
2026-09-06 13:41:55 +0530

    Add files via upload

---
+++ avgof2number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+float a,b;
+    printf("enter the first number:");
+scanf("%f", &a);
+
+ printf("enter the second number:");
+scanf("%f", &b);
+ float c=a+b;
+ float d=c/2;
+ printf("the average of your number is: %f",d);
+
+     return 0;
+}
